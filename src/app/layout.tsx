ad009fee47b60f172dae372cfd34f8679deac17c
Maxime Lebreton <maxime@lebreton.pro>
2024-09-02 09:09:08 +0200

    add clickable link to Github source code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,7 @@ import { DownloadOutlined, GithubFilled, QuestionCircleFilled } from "@ant-desig
 import Wallet from "@/components/Wallet";
 import { WalletProvider } from "@/context/walletContext";
 import StyledJsxRegistry from "./registry";
+import Link from "next/link";
 
 const ubuntu = Ubuntu({ subsets: ["latin"], weight: "300" });
 
@@ -54,52 +55,46 @@ export default function RootLayout({
   return (
     <html lang="en" className={ubuntu.className}>
       <Body>
-      <StyledJsxRegistry>
-        <AntdRegistry>
-          <WalletProvider>
-            <Row style={{ width: "100%" }}>
-              <StyledCol
-                xs={{ span: 22, offset: 1 }}
-                sm={{ span: 20, offset: 2 }}
-                md={{ span: 16, offset: 4 }}
-                lg={{ span: 12, offset: 6 }}
-                xl={{ span: 8, offset: 8 }}
-              >
-                <Row>
-                  <Col xs={24} sm={12}>
-                    <span style={{color:"white", fontSize:"24px", fontWeight : "bold"}}>
-                      TBDA
-                    </span>
-                  </Col>
-                  <TitleCol xs={24} sm={12}>
-                    <Wallet />
-                  </TitleCol>
-                </Row>
-                <Row style={{marginTop : "16px"}}>
-                  <Col span={24}>
-                    <StyledCard
-                      bordered={false}
-                      title={
-                        <Space align="baseline">
-                          <DownloadOutlined style={{ fontSize: "20px" }} />
-                          <Title level={4}> Deposit</Title>
-                        </Space>
-                      }
-                    >
-                      {children}
-                    </StyledCard>
-                  </Col>
-                </Row>
-                <Row>
-                  <IconSpace>
-                    <QuestionCircleFilled style={{ color: "white", fontSize: "24px" }} />
-                    <GithubFilled style={{ color: "white", fontSize: "24px" }} />
-                  </IconSpace>
-                </Row>
-              </StyledCol>
-            </Row>
-          </WalletProvider>
-        </AntdRegistry>
+        <StyledJsxRegistry>
+          <AntdRegistry>
+            <WalletProvider>
+              <Row style={{ width: "100%" }}>
+                <StyledCol xs={{ span: 22, offset: 1 }} sm={{ span: 20, offset: 2 }} md={{ span: 16, offset: 4 }} lg={{ span: 12, offset: 6 }} xl={{ span: 8, offset: 8 }}>
+                  <Row>
+                    <Col xs={24} sm={12}>
+                      <span style={{ color: "white", fontSize: "24px", fontWeight: "bold" }}>TBDA</span>
+                    </Col>
+                    <TitleCol xs={24} sm={12}>
+                      <Wallet />
+                    </TitleCol>
+                  </Row>
+                  <Row style={{ marginTop: "16px" }}>
+                    <Col span={24}>
+                      <StyledCard
+                        bordered={false}
+                        title={
+                          <Space align="baseline">
+                            <DownloadOutlined style={{ fontSize: "20px" }} />
+                            <Title level={4}> Deposit</Title>
+                          </Space>
+                        }
+                      >
+                        {children}
+                      </StyledCard>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <IconSpace>
+                      <QuestionCircleFilled style={{ color: "white", fontSize: "24px" }} />
+                      <Link href={"https://github.com/Maxime-LBT/testnet-bitcoin-deposit-nextjs"}>
+                        <GithubFilled style={{ color: "white", fontSize: "24px" }} />
+                      </Link>
+                    </IconSpace>
+                  </Row>
+                </StyledCol>
+              </Row>
+            </WalletProvider>
+          </AntdRegistry>
         </StyledJsxRegistry>
       </Body>
     </html>
