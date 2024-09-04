66007eb00f8db48485037b4226c267b24b58a33c
Maxime Lebreton <maxime@lebreton.pro>
2024-09-04 12:05:37 +0200

    refactor ErrorStep step to component

---
+++ src/components/ErrorStep/index.tsx
@@ -0,0 +1,30 @@
+import React, { FC } from 'react';
+import { Button, Col, Result, Row } from 'antd';
+import styled from 'styled-components';
+
+const CenteredRow = styled(Row)`
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  margin: auto;
+  text-align: center;
+`;
+
+const ErrorStep: FC<{ onBack: () => void }> = ({ onBack }) => (
+  <CenteredRow>
+    <Col span={24}>
+      <Result
+        status="error"
+        title="Transaction Error"
+        subTitle="An issue was encountered while processing your payment. Please try again later."
+        extra={[
+          <Button key="deposit" onClick={onBack}>
+            Try Again
+          </Button>,
+        ]}
+      />
+    </Col>
+  </CenteredRow>
+);
+
+export default ErrorStep;
