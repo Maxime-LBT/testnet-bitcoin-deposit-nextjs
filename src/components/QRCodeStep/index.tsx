d81b1c8e5b4ef7cdf77d902daaff351e451bce65
Maxime Lebreton <maxime@lebreton.pro>
2024-09-04 12:56:35 +0200

    bette responsive design

--- src/components/QRCodeStep/index.tsx
+++ src/components/QRCodeStep/index.tsx
@@ -1,7 +1,8 @@
 import React, { FC } from 'react';
 import QRCode from 'react-qr-code';
-import { Button, Tooltip, Row, Col, Input, notification } from 'antd';
-import { CopyOutlined } from '@ant-design/icons';
+import { Button, Tooltip, Row, Col, Input, notification, Space } from 'antd';
+import Text from 'antd/es/typography/Text';
+import { CopyOutlined, ExclamationCircleFilled } from '@ant-design/icons';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 
@@ -11,27 +12,8 @@ interface QRCodeStepProps {
   onCancel: () => void;
 }
 
-const StyledRow = styled(Row)`
-  width: 100%;
-  margin: auto;
-`;
-
-const StyledCol = styled(Col)`
-  display: flex;
-  align-items: center;
-  text-align: right;
-`;
-
-const FullWidthRow = styled(Row)`
-  width: 100%;
-`;
-
 const FullWidthInput = styled(Input)`
   width: 100%;
-`;
-
-const RightAlignedCol = styled(Col)`
-  text-align: right;
 `;
 
 const StyledQRCode = styled(QRCode)`
@@ -42,9 +24,17 @@ const StyledQRCode = styled(QRCode)`
 const QRCodeWrapper = styled(Col)`
   span {
     display: block;
-    margin-top: 10px;
     text-align: center;
   }
+`;
+
+const LeftAlignedCol = styled(Col)`
+  align-items: left;
+`;
+
+const CancelButton = styled(Button)`
+  background-color: #f5222d !important;
+  border-color: #f5222d !important;
 `;
 
 const QRCodeStep: FC<QRCodeStepProps> = ({ paymentRequest, btcAmount, onCancel }) => {
@@ -56,27 +46,32 @@ const QRCodeStep: FC<QRCodeStepProps> = ({ paymentRequest, btcAmount, onCancel }
   return (
     <motion.div key="showQRCode" initial={{ x: -100, opacity: 0 }} animate={{ x: 0, opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.1 }}>
       <QRCodeWrapper>
-        <StyledRow gutter={[16, 16]}>
-          <StyledCol xs={{ span: 16, offset: 4 }} sm={{ span: 14, offset: 5 }} md={{ span: 14, offset: 5 }} lg={{ span: 14, offset: 5 }} xl={{ span: 14, offset: 5 }}>
-            <FullWidthRow>
-              <Col span={20}>
-                <FullWidthInput value={paymentRequest} readOnly />
-              </Col>
-              <RightAlignedCol span={4}>
-                <Tooltip title="Copy Address">
-                  <Button size="middle" icon={<CopyOutlined />} onClick={copyToClipboard} aria-label="Copy Address" />
-                </Tooltip>
-              </RightAlignedCol>
-            </FullWidthRow>
-          </StyledCol>
-          <QRCodeWrapper xs={{ span: 16, offset: 4 }} sm={{ span: 14, offset: 5 }} md={{ span: 14, offset: 5 }} lg={{ span: 14, offset: 5 }} xl={{ span: 14, offset: 5 }}>
+        <Row gutter={[16, 8]}>
+          <LeftAlignedCol span={24}>
+            <Space align="center">
+              <Tooltip title="The payment will be autodetected">
+                <ExclamationCircleFilled style={{ color: '#1677ff', fontSize: '20px' }} />
+              </Tooltip>
+              <Text style={{ fontSize: '16px' }}>Scan and send {btcAmount} BTC</Text>
+            </Space>
+          </LeftAlignedCol>
+          <QRCodeWrapper xs={{ span: 14, offset: 5 }} sm={{ span: 12, offset: 6 }} md={{ span: 12, offset: 6 }} lg={{ span: 12, offset: 6 }} xl={{ span: 10, offset: 7 }}>
             <StyledQRCode data-testid="qr-code-display" id="qr-code-display" name="qr-code-display" value={paymentRequest} />
-            <span>Scan and send {btcAmount} BTC</span>
           </QRCodeWrapper>
-        </StyledRow>
-        <Button type="primary" style={{ backgroundColor: '#f5222d', borderColor: '#f5222d' }} onClick={onCancel}>
-          Cancel
-        </Button>
+          <Col xs={{ span: 12, offset: 6 }} sm={{ span: 12, offset: 6 }} md={{ span: 12, offset: 6 }} lg={{ span: 12, offset: 6 }} xl={{ span: 12, offset: 6 }}>
+            <Space>
+              <FullWidthInput size="small" value={paymentRequest} readOnly />
+              <Tooltip title="Copy Address">
+                <Button size="small" icon={<CopyOutlined />} onClick={copyToClipboard} aria-label="Copy Address" />
+              </Tooltip>
+            </Space>
+          </Col>
+          <Col xs={{ span: 16, offset: 4 }} sm={{ span: 14, offset: 5 }} md={{ span: 14, offset: 5 }} lg={{ span: 14, offset: 5 }} xl={{ span: 14, offset: 5 }}>
+            <CancelButton type="primary" onClick={onCancel}>
+              Cancel
+            </CancelButton>
+          </Col>
+        </Row>
       </QRCodeWrapper>
     </motion.div>
   );
