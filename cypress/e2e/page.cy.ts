78cd4e549e3cf5e4689485b3f3dbced3737fa6e8
Maxime Lebreton <maxime@lebreton.pro>
2024-09-04 17:22:52 +0200

    add delay for loading

--- cypress/e2e/page.cy.ts
+++ cypress/e2e/page.cy.ts
@@ -1,29 +1,29 @@
 import { cy } from 'local-cypress';
 
 describe('Home Page Payment Flow', () => {
-    beforeEach(() => {
-        // Visit the page before each test
-        cy.visit('http://localhost:3000/')
-    });
+  beforeEach(() => {
+    // Visit the page before each test
+    cy.visit('http://localhost:3000/');
+  });
 
-    it('should load the home page', () => {
-        cy.url().should('eq', 'http://localhost:3000/');
-    });
+  it('should load the home page', () => {
+    cy.url().should('eq', 'http://localhost:3000/');
+  });
 
-    it('should enter an amount, submit the form, and cancel the transaction', () => {
-        // Enter the amount into the input field
-        cy.get('input[id="amount"]').type('0.0001');
-    
-        // Click the submit button to generate the payment QR code
-        cy.get('button[type="submit"]').click();
-    
-        // Verify that the QR code is displayed
-        cy.get('[id="qr-code-display"]').should('be.visible');
+  it('should enter an amount, submit the form, and cancel the transaction', () => {
+    // Enter the amount into the input field
+    cy.get('input[id="amount"]').type('0.0001');
 
-        // Simulate cancel
-        cy.get('button[id="cancel-transaction"]').click();
-    
-        // Verify that the QR code is no longer displayed
-        cy.get('[id="qr-code-display"]').should('not.exist');
-    });
-})
+    // Click the submit button to generate the payment QR code
+    cy.get('button[type="submit"]').click();
+
+    // Verify that the QR code is displayed
+    cy.get('[id="qr-code-display"]').should('be.visible');
+
+    cy.wait(1000); // wait for 1 second
+    cy.get('button[id="cancel-transaction"]').click();
+
+    // Verify that the QR code is no longer displayed
+    cy.get('[id="qr-code-display"]').should('not.exist');
+  });
+});
