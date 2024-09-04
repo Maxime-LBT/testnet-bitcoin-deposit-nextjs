9aa51ebf2e69f98cd79452188edb79b6507fe549
Maxime Lebreton <maxime@lebreton.pro>
2024-09-04 13:19:34 +0200

    add blockchain explorer url in env

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_POLLING_INTERVAL: process.env.POLLING_INTERVAL,
+    NEXT_PUBLIC_EXPLORER_API_URL: process.env.EXPLORER_API_URL,
   },
   logging: {
     fetches: {
@@ -13,8 +14,8 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
-    webpack: (config) => {
-    config.externals.push("pino-pretty", "lokijs", "encoding");
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
     return config;
   },
 };
