bb6b8a6cf2e46d08bd6fca026d1faeeb3686a23b
Maxime Lebreton <maxime@lebreton.pro>
2024-09-04 10:48:29 +0200

    remove logging metadata

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -2,21 +2,18 @@ import { createLogger, format, transports, Logger } from 'winston';
 
 // Define the log format
 const logFormat = format.combine(
-    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-    format.printf(({ timestamp, level, message, ...metadata }) => {
-        let log = `${timestamp} [${level}] : ${message} `;
-        if (Object.keys(metadata).length) {
-            log += JSON.stringify(metadata);
-        }
-        return log;
-    }),
+  format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+  format.printf(({ timestamp, level, message }) => {
+    const log = `${timestamp} [${level}] : ${message} `;
+    return log;
+  }),
 );
 
 // Create a Winston logger instance
 const logger: Logger = createLogger({
-    level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
-    format: logFormat,
-    transports: [new transports.Console()],
+  level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+  format: logFormat,
+  transports: [new transports.Console()],
 });
 
 export default logger;
