78620b723b720d53122f7e906474a3846a8ae1a8
Maxime Lebreton <maxime@lebreton.pro>
2024-09-04 10:49:28 +0200

    prettier fix

--- src/components/RequestForm/index.tsx
+++ src/components/RequestForm/index.tsx
@@ -3,50 +3,50 @@ import { Button, Input, Row, Col, Form } from 'antd';
 import styled from 'styled-components';
 
 interface RequestFormProps {
-    onSubmit: (btcAmount: number) => void;
+  onSubmit: (btcAmount: number) => void;
 }
 
 const StyledRow = styled(Row)`
-    width: 100%;
-    margin: auto;
-    text-align: center;
+  width: 100%;
+  margin: auto;
+  text-align: center;
 `;
 
 const StyledCol = styled(Col)`
-    text-align: left;
+  text-align: left;
 `;
 
 const StyledButton = styled(Button)`
-    width: 100%;
+  width: 100%;
 `;
 
 const RequestForm: React.FC<RequestFormProps> = ({ onSubmit }) => {
-    const [amount, setAmount] = useState<number>(0.0001);
+  const [amount, setAmount] = useState<number>(0.0001);
 
-    const handleSubmit = (values: { amount: number }) => {
-        onSubmit(values.amount);
-    };
+  const handleSubmit = (values: { amount: number }) => {
+    onSubmit(values.amount);
+  };
 
-    const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setAmount(parseFloat(e.target.value) || 0);
-    };
+  const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAmount(parseFloat(e.target.value) || 0);
+  };
 
-    return (
-        <StyledRow align="middle" justify="center">
-            <StyledCol span={24}>
-                <Form layout="vertical" onFinish={handleSubmit}>
-                    <Form.Item label="Enter the amount you want to deposit:" name="amount" rules={[{ required: true, message: 'Please input the amount!' }]}>
-                        <Input suffix="tBTC" type="number" step="0.00001" min="0.00001" size="middle" value={amount} onChange={handleAmountChange} placeholder="0.00001" />
-                    </Form.Item>
-                    <Form.Item>
-                        <StyledButton type="primary" htmlType="submit" size="middle">
-                            Generate Payment QR Code
-                        </StyledButton>
-                    </Form.Item>
-                </Form>
-            </StyledCol>
-        </StyledRow>
-    );
+  return (
+    <StyledRow align="middle" justify="center">
+      <StyledCol span={24}>
+        <Form layout="vertical" onFinish={handleSubmit}>
+          <Form.Item label="Enter the amount you want to deposit:" name="amount" rules={[{ required: true, message: 'Please input the amount!' }]}>
+            <Input suffix="tBTC" type="number" step="0.00001" min="0.00001" size="middle" value={amount} onChange={handleAmountChange} placeholder="0.00001" />
+          </Form.Item>
+          <Form.Item>
+            <StyledButton type="primary" htmlType="submit" size="middle">
+              Generate Payment QR Code
+            </StyledButton>
+          </Form.Item>
+        </Form>
+      </StyledCol>
+    </StyledRow>
+  );
 };
 
 export default RequestForm;
